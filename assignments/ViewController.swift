acac2ded1926e65e671d7bcfdcd6bb5889f078ef
ptamayo25 <priscillatamayoeit@gmail.com>
2017-09-04 14:27:40 -0700

    setup xcode project

---
+++ assignments/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Assignments
+//
+//  Created by Priscilla Tamayo on 9/4/17.
+//  Copyright © 2017 Priscilla Tamayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
