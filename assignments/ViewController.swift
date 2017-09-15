32f7890a50e5dbe69d9c18ea4c897b677d679c78
ptamayo25 <priscillatamayoeit@gmail.com>
2017-09-14 21:39:20 -0700

    Viewcontroller 4 colored tiles

--- assignments/ViewController.swift
+++ assignments/ViewController.swift
@@ -12,10 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let viewwidth =  self.view.frame.size.width
+        let viewheight = self.view.frame.size.height
+      //orange view tile set up
+        let orangeTileFrameWidth = CGFloat (50)
+        let orangeTileFrameHeight = CGFloat (50)
+        let orangeX = CGFloat(0)
+        let orangeY = CGFloat(0)
+        
+        
+        let orangeTileFrame = CGRect(x: orangeX, y: orangeY, width: orangeTileFrameWidth, height: orangeTileFrameHeight)
+        let orangeTile = UIView(frame: orangeTileFrame)
+        orangeTile.backgroundColor = UIColor.orange
+        self.view.addSubview(orangeTile)
+        
+      // blue view tile setup
+        let blueTileFrameWidth = CGFloat (50)
+        let blueTileFrameHeight = CGFloat (50)
+        let blueX = viewwidth - blueTileFrameWidth
+        let blueY = CGFloat(0)
+        
+        let blueTileFrame = CGRect(x: blueX, y: CGFloat(blueY), width: blueTileFrameWidth, height: blueTileFrameHeight)
+        let blueTile = UIView(frame: blueTileFrame)
+        blueTile.backgroundColor = UIColor.blue
+        self.view.addSubview(blueTile)
+        
+      // red view tile setup
+        let redTileFrameWidth = CGFloat (50)
+        let redTileFrameHeight = CGFloat (50)
+        let redX = viewwidth - redTileFrameWidth
+        let redY = viewheight - redTileFrameHeight
+        
+        let redTileFrame = CGRect(x: redX, y: CGFloat(redY), width: redTileFrameWidth, height: redTileFrameHeight)
+        let redTile = UIView(frame: redTileFrame)
+        redTile.backgroundColor = UIColor.red
+        self.view.addSubview(redTile)
+        
+      // green view tile setup
+        let greenTileFrameWidth = CGFloat (50)
+        let greenTileFrameHeight = CGFloat (50)
+        let greenX = CGFloat(0)
+        let greenY = viewheight - redTileFrameHeight
+        
+        let greenTileFrame = CGRect(x: greenX, y: CGFloat(greenY), width: greenTileFrameWidth, height: greenTileFrameHeight)
+        let greenTile = UIView(frame: greenTileFrame)
+        greenTile.backgroundColor = UIColor.green
+        self.view.addSubview(greenTile)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         // Dispose of any resources that can be recreated.
     }
 
